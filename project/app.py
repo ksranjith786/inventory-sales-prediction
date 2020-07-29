fe8db387d2caf1160fd0672ddab7e9fb25084061
ksranjith786 <ksranjith786@gmail.com>
2020-07-29 19:16:09 +0530

    C:/Program Files/Git/predict API working

--- project/app.py
+++ project/app.py
@@ -1,16 +1,11 @@
 from flask import Flask, Blueprint, jsonify
 from os import environ
-from sqlalchemy import *
-from flask_sqlalchemy import SQLAlchemy
-import urllib, sqlalchemy
 import pandas as pd 
 
 from routes.home import home_bp
 
 blueprints = (home_bp,)
 app = Flask(__name__)
-connstr="DRIVER={ODBC Driver 17 for SQL Server};SERVER=hackathon2020-paymentors.database.windows.net;DATABASE=RetailInventory;UID=paymentors;PWD=ncrhyd2020@";
-db = SQLAlchemy()
 
 def get_config(app):
     app.config.from_pyfile('config.py', silent=True)
@@ -34,23 +29,8 @@ def register_blueprint(app, blueprints):
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    """
-    params = urllib.parse.quote(connstr)
-    engine = sqlalchemy.create_engine('mssql+pyodbc:///?odbc_connect={}'.format(params))
-    conn = engine.connect()
-    
-    result = conn.execute("SELECT TOP (10) * FROM [dbo].[transactions]")
-    for row in result:
-        print (str(row[2]) + " " + str(row[3]))
-    
-    conn.close()
-    """
-    
-    from models.transaction import Transaction
     PREDICT_DATASET='data/predict.csv'
     df = pd.read_csv(PREDICT_DATASET)
-    #db.session()
-    #trans = Transaction.query.filter_by(TransactionID="CA-2016-152156").first_or_404()
     return jsonify(df.to_dict('records'))
 # end predict
 
--- project/config.py
+++ project/config.py
@@ -1,7 +1,5 @@
 import os 
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-SQLALCHEMY_DATABASE_URI="DRIVER={ODBC Driver 17 for SQL Server};SERVER=hackathon2020-paymentors.database.windows.net;DATABASE=RetailInventory;UID=paymentors;PWD=ncrhyd2020@"
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-#SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-#@"Server=tcp:hackathon2020-paymentors.database.windows.net,1433;Initial Catalog=RetailInventory;Persist Security Info=False;User ID=paymentors;Password=ncrhyd2020@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+#SQLALCHEMY_TRACK_MODIFICATIONS = False
