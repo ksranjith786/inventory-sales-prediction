92dedbed300adab06e6948d753a9419ce122a5f4
ksranjith786 <ksranjith786@gmail.com>
2020-07-29 14:37:21 +0530

    removed fbprophet

--- project/routes/predict.py
+++ project/routes/predict.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template
-
+from fbprophet import Prophet
 predict_bp = Blueprint('predict', __name__, url_prefix='/predict')
 
 @predict_bp.route('/', methods=['GET', 'POST'])
 def predict():
+    print(dir(Prophet))
     return "Prediction in Progress"
 # end predict
